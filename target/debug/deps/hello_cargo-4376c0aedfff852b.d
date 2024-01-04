96b5367ad01a4c3584f8701b3b19d9685d93dbe9
sanketh <sankethbm333@gmail.com>
2024-01-05 00:04:02 +0530

    first commit

---
+++ target/debug/deps/hello_cargo-4376c0aedfff852b.d
@@ -0,0 +1,5 @@
+/home/sanketh/Desktop/rust_practice/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b: src/main.rs
+
+/home/sanketh/Desktop/rust_practice/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/sanketh/Desktop/rust_practice/hello_cargo/target/debug/hello_cargo: /home/sanketh/Desktop/rust_practice/hello_cargo/src/main.rs
