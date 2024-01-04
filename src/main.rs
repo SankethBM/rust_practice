9f8c42fa836b7765bd1ecd99b92b8b956ae9e5bd
sanketh <sankethbm333@gmail.com>
2024-01-05 00:11:46 +0530

    5th commit

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,19 @@ fn main() {
         let mut g = String::new();
         io::stdin().read_line(&mut g).expect("failed");
         println!("you gussed {}", g);
+
+        let g: u32 = g.trim().parse().expect("Enter a no");
+
+        let sn = rand::thread_rng().gen_range(1, 101);
+        println!("Generated no : {}", sn);
+        match g.cmp(&sn) {
+            Ordering::Less => println!("{}", "small".red()),
+            Ordering::Greater => println!("{}", "big".red()),
+            Ordering::Equal => {
+                println!("{}", "You win".green());
+                break;
+            }
+        }
     }
 }
 
